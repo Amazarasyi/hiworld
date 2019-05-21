7552fbea3b04d0983276200eb679e5515eb8d761
Amazarasyi <414803223@qq.com>
2019-05-21 21:09:11 +0800

    package

--- src/Service.java
+++ src/Service.java
@@ -1,5 +1,8 @@
 public class Service {
     public static void main(String[] args) {
         System.out.println("1.0修复");
+        System.out.println("ad");
+        System.out.println("work");
+
     }
 }
